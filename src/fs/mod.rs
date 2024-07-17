88c98c28a1a08d216658afd8f455489019711958
Philip Johansson <kjughx@gmail.com>
2024-07-17 10:42:22 +0000

    Use FAT16 as VFS to resolve our disk 0

--- src/fs/mod.rs
+++ src/fs/mod.rs
@@ -1,11 +1,15 @@
 use crate::{
     boxed::Box,
+    disk::{Disk, Stream},
+    lock,
     path::Path,
+    sync::Global,
 };
 
 use core::any::Any;
 
 mod filesystems;
+use filesystems::fat16::Fat16;
 
 #[derive(Debug)]
 pub enum FSError {
@@ -35,8 +39,9 @@ pub enum SeekMode {
 pub trait FileSystem {
     fn open(
         &self,
+        stream: &mut dyn Stream,
         path: Path,
-        mode: FileMode,
+        _mode: FileMode,
     ) -> Result<Box<dyn FileDescriptor>, IOError>;
     fn read(&self, fd: Box<dyn FileDescriptor>);
     fn seek(&self);
@@ -52,3 +57,34 @@ pub trait FileDescriptor {
     fn seek(&mut self, offset: isize, whence: SeekMode);
 }
 
+pub struct Vfs;
+impl Vfs {
+    pub fn resolve(disk: &mut Global<Disk>) -> Result<(), FSError> {
+        match Fat16::resolve(disk) {
+            Ok(fs) => {
+                lock!(disk).register_filesystem(fs);
+                return Ok(());
+            }
+            Err(FSError::NotOurFS) => (),
+            Err(e) => Err(e)?,
+        }
+
+        Err(FSError::FSNotFound)
+    }
+
+    pub fn open(path: Path, mode: FileMode) -> Result<Box<dyn FileDescriptor>, IOError> {
+        let Some(disk_id) = path.disk_id else {
+            return Err(IOError::InvalidDisk);
+        };
+
+        let disk = lock!(Disk::get(disk_id));
+
+        let Some(ref fs) = disk.filesystem else {
+            return Err(IOError::NoFS);
+        };
+
+        let mut stream = disk.stream();
+
+        fs.open(&mut stream, path, mode)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@
 use core::hint;
 
 use ruix::{
+    disk::Disk,
+    fs::Vfs,
     traceln,
     tty::{init_screen, print},
 };
@@ -11,6 +13,13 @@ use ruix::{
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn kernel_main() -> ! {
     init_screen();
+
+    // Resolve the connected disks
+    match Vfs::resolve(Disk::get_mut(0)) {
+        Ok(()) => (),
+        Err(_) => print("Could not resolve disk 0"),
+    }
+
     print("Hello, World!");
     traceln!("Hello, World!");
     loop {
