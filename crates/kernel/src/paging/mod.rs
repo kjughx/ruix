6c12525e22351e2e03146f2b03481add321b1056
Philip J. <kjughx@gmail.com>
2025-02-08 16:27:55 +0100

    Implement Display for Addr

--- crates/kernel/src/paging/mod.rs
+++ crates/kernel/src/paging/mod.rs
@@ -108,6 +108,12 @@ impl Addr {
     }
 }
 
+impl core::fmt::Display for Addr {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "{:x}", self.0)
+    }
+}
+
 pub struct Page(pub usize);
 pub struct Offset(pub usize);
 
